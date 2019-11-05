2cc8441cbe8228f80f55c5dae93fa700a5529567
Hoarder_PC <18763137197@163.com>
2019-11-05 20:56:54 +0800

    add project structure

---
+++ Base/src/main/java/com/joven/base/common/BaseConstants.java
@@ -0,0 +1,8 @@
+package com.joven.base.common;
+
+/**
+ * 一些的常量类
+ */
+public final class BaseConstants {
+
+}
--- Base/src/main/java/com/joven/base/controller/DailyExpensesInfo.java
+++ Base/src/main/java/com/joven/base/controller/DailyExpensesInfo.java
@@ -7,7 +7,6 @@ import com.joven.base.service.impl.DailyExpensesInfoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
