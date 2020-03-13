f48e3eed16835d74295aad4b99af20e4345e4710
Joven-20171012 <18763137197@163.com>
2020-03-14 00:15:57 +0800

    add maven project

---
+++ mavenTimetravle/base/tools/src/main/java/org/joven/base/tools/common/ToolConstants.java
@@ -0,0 +1,17 @@
+/**
+ * Project Name: ershuaizhang.github.io
+ * File Name: ToolConstants
+ * Package Name: org.joven.base.tools.common
+ * Date: 2020/3/13 23:27
+ * Copyright (c) 2020,All Rights Reserved.
+ */
+package org.joven.base.tools.common;
+
+/**
+ * CreateBy Administrator
+ * Date: 2020/3/13 23:27
+ * Version: 
+ * Remark: 
+ */
+public class ToolConstants {
+}
