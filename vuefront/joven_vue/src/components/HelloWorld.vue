c8f3faa4e432797fd2d2ccd66034b799e00f9153
Joven-20171012 <18763137197@163.com>
2020-06-26 17:26:10 +0800

    modify vue demo and update base js

--- vuefront/joven_vue/src/components/HelloWorld.vue
+++ vuefront/joven_vue/src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<template>
+﻿<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -96,18 +96,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
