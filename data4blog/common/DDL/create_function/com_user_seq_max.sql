6f79230b3a8cc78b49b9a42709e6c742122c75d9
zhangershuai <18763137197@163.com>
2022-02-13 15:49:46 +0800

    new action

--- data4blog/common/DDL/create_function/com_user_seq_max.sql
+++
@@ -1,9 +0,0 @@
-create
-    definer = com_user@`%` function com_user.get_seq_max(seq_name char(100)) returns bigint
-BEGIN
-    DECLARE max BIGINT;
-    SET max = 0;
-    SELECT max_value into max FROM t_sequence WHERE name = seq_name;
-    RETURN max;
-END;
-
